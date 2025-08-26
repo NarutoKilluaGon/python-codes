e6f2d7170c3eb264799e723f6e04372965d6ab91
NarutoKilluaGon <devvatsa192005@gmail.com>
2025-08-26 18:38:52 +0530

    demo commit 3

--- demo.py
+++ demo.py
@@ -10,3 +10,5 @@ sum = num1 + num2
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 #this is comment del later
 #meowtie
+#lollolloolololololololol hahahahahahh yoyoooyoyoyoyo
+a="string"
